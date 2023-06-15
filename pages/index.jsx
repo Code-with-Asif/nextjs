882c17f7a10540f2d16c316ab3b96218ec85355a
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-16 01:27:04 +0500

    make changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+import styles from "../styles/Home.module.css";
 const Random = () => {
-  return <div>Random</div>;
+  let heading = "Beautiful Websites Templates";
+  let para = "lets make some beautiful tamplates";
+  let btn1 = "Download";
+  let btn2 = "Get Started";
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>{heading}</h1>
+        <p>{para}</p>
+        <div className={styles.btns}>
+          {" "}
+          <button>{btn1}</button>
+          <button>{btn2}</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Random;
