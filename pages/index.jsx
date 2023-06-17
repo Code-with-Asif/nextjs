762c99c1b744a08ec4de3b8f956248f7e88d44c1
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-17 21:41:01 +0500

    make changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,16 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 const Random = () => {
-  let heading = "Beautiful Websites Templates";
-  let para = "lets make some beautiful tamplates";
-  let btn1 = "Download";
-  let btn2 = "Get Started";
+  const [value, setValue] = useState(0);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>{heading}</h1>
-        <p>{para}</p>
+        <h1>{value}</h1>
         <div className={styles.btns}>
-          {" "}
-          <button>{btn1}</button>
-          <button>{btn2}</button>
+          <button>Increment</button>
+          <button>Increment</button>
+          <button>Increment</button>
         </div>
       </div>
     </div>
