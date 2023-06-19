6516bcbeb9e082111841adaef2a39dc1e8602d51
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-20 01:08:06 +0500

    made changes

--- pages/object.jsx
+++ pages/object.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
+import styles from "../styles/Home.module.css";
 
 const Object = () => {
-  [user, setUser] = useState({});
-  return <div></div>;
+  const [user, setUser] = useState({
+    name: "Ali",
+    age: 25,
+    message: "welcome  user",
+  });
+  return (
+    <div className={styles.userData}>
+      <h1>{user.name}</h1>
+      <h2>{user.age}</h2>
+      <h3>{user.message}</h3>
+      <button>Change </button>
+    </div>
+  );
 };
 
 export default Object;
