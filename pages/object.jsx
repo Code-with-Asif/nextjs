ad9925180e24d96277ed9a0a1c990c2a3447f616
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-21 00:35:05 +0500

    made changes

--- pages/object.jsx
+++ pages/object.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Object = () => {
+  const [info, setInfo] = useState([]);
   const [user, setUser] = useState({
     name: "Ali",
     age: 25,
     message: "welcome  user",
   });
+  const textChange = () => {
+    setUser({ ...user, message: "welcome Asif" });
+  };
   return (
     <div className={styles.userData}>
       <h1>{user.name}</h1>
       <h2>{user.age}</h2>
       <h3>{user.message}</h3>
-      <button>Change </button>
+      <button onClick={textChange}>Change </button>
     </div>
   );
 };
