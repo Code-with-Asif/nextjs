0d1b824f060303d2fa4e8cbdef40495210bd7085
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-21 22:48:08 +0500

    made changes

---
+++ pages/data.js
@@ -0,0 +1,6 @@
+export const users = [
+  { name: "Ali", age: 20 },
+  { name: "Asif", age: 22 },
+  { name: "jhon", age: 30 },
+  { name: "james", age: 28 },
+];
--- pages/object.jsx
+++ pages/object.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
+import { users } from "./data";
 
 const Object = () => {
-  const [info, setInfo] = useState([]);
-  const [user, setUser] = useState({
-    name: "Ali",
-    age: 25,
-    message: "welcome  user",
-  });
-  const textChange = () => {
-    setUser({ ...user, message: "welcome Asif" });
-  };
+  const [info, setInfo] = useState(users);
+  // const [user, setUser] = useState({
+  //   name: "Ali",
+  //   age: 25,
+  //   message: "welcome  user",
+  // });
+  // const textChange = () => {
+  //   setUser({ ...user, message: "welcome Asif" });
+  // };
   return (
     <div className={styles.userData}>
-      <h1>{user.name}</h1>
+      {/* <h1>{user.name}</h1>
       <h2>{user.age}</h2>
       <h3>{user.message}</h3>
-      <button onClick={textChange}>Change </button>
+      <button onClick={textChange}>Change </button> */}
+
+      {info.map((myUser) => {
+        return (
+          <div className={styles.myUser}>
+            <h1>{myUser.name}</h1>
+            <h1>{myUser.age}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 };
